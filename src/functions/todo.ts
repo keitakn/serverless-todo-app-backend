bdb5b3d1489e25914f2084a003c6a6772767cbe2
KeitaNishimoto <k.mail.09070876640@icloud.com>
2017-07-02 04:24:46 +0900

    modify #1 正常系のテストが通るように関数を実装

--- src/functions/todo.ts
+++ src/functions/todo.ts
@@ -1,7 +1,14 @@
 import * as lambda from "aws-lambda";
 import * as sourceMapSupport from "source-map-support";
+import * as uuidV4 from "uuid/v4";
+import ErrorResponse from "../domain/ErrorResponse";
+import AwsSdkFactory from "../factories/AwsSdkFactory";
+import RequestFactory from "../factories/RequestFactory";
+import TodoRepository from "../repositories/TodoRepository";
 
 sourceMapSupport.install();
+
+const dynamoDbDocumentClient = AwsSdkFactory.createDynamoDbDocumentClient();
 
 /**
  * TODOを追加する
@@ -16,15 +23,33 @@ export const create: lambda.ProxyHandler = async (
   context: lambda.Context,
   callback: lambda.Callback,
 ): Promise<void> => {
+  try {
+    const requestObject: TodoRequest.CreateRequest = new RequestFactory(event).create();
 
-  const response = {
-    statusCode: 201,
-    body: JSON.stringify({
-      id: 1,
-      title: "買い物に行く",
-    }),
-    headers: {"Access-Control-Allow-Origin": "*"},
-  };
+    const nowDateTime = new Date().getTime();
 
-  callback(undefined, response);
+    const createParams = {
+      id: uuidV4(),
+      title: requestObject.title,
+      isCompleted: false,
+      createdAt: nowDateTime,
+      updatedAt: nowDateTime,
+    };
+
+    const todoRepository = new TodoRepository(dynamoDbDocumentClient);
+    const createResponse = await todoRepository.create(createParams);
+
+    const response = {
+      statusCode: 201,
+      body: JSON.stringify(createResponse),
+      headers: {"Access-Control-Allow-Origin": "*"},
+    };
+
+    callback(undefined, response);
+  } catch (error) {
+    const errorResponse = new ErrorResponse(error);
+    const response = errorResponse.getResponse();
+
+    callback(undefined, response);
+  }
 };
