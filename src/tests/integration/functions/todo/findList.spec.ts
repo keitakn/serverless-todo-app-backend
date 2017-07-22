75ee804acca10985a8d13e2a3065528a5c9c9a81
keita-nishimoto <k.mail.09070876640@icloud.com>
2017-07-22 17:39:29 +0900

    add #3 Queryパラメータを指定しない際のテストを追加

--- src/tests/integration/functions/todo/findList.spec.ts
+++ src/tests/integration/functions/todo/findList.spec.ts
@@ -38,6 +38,7 @@ describe("findTodoListTest", () => {
 
   /**
    * 正常系テストケース
+   * Queryパラメータを指定する
    */
   it("testSuccessSetQueryParams", () => {
     return (async () => {
@@ -48,6 +49,22 @@ describe("findTodoListTest", () => {
       const response = await TodoTest.ApiClient.findTodoList(request);
       assert.equal(response.status, 200, "ステータスコードのチェック");
       assert.equal(response.data.count, 1, "件数のチェック");
+      assert.ok(response.data.items, "itemsが設定されている事をチェック");
+    })().catch((error) => {
+      assert.fail(error.response.data);
+    });
+  });
+
+  /**
+   * 正常系テストケース
+   * Queryパラメータを指定しない
+   */
+  it("testSuccessNoSetQueryParams", () => {
+    return (async () => {
+      const response = await TodoTest.ApiClient.findTodoList();
+      assert.equal(response.status, 200, "ステータスコードのチェック");
+      assert.ok(response.data.count, "countが設定されている事をチェック");
+      assert.ok(response.data.items, "itemsが設定されている事をチェック");
     })().catch((error) => {
       assert.fail(error.response.data);
     });
